a1fbb9012fd6e741064bb243992aae3c8777c4cb
AndreEstz <andrerod.est@gmail.com>
2024-05-05 13:33:02 -0300

    Quadro medidas resumo

--- rdocs/Analise.R
+++ rdocs/Analise.R
@@ -46,7 +46,83 @@ theme_estat <- function(...) {
     )
   )
 }
+
 caminho_andre <- 'resultados'
+
+print_quadro_resumo <- function(data, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+{
+  data <- data %>%
+    summarize(`Média` = round(mean(imdb),2),
+              `Desvio Padrão` = round(sd(imdb),2),
+              `Variância` = round(var(imdb),2),
+              `Mínimo` = round(min(imdb),2),
+              `1º Quartil` = round(quantile(imdb, probs = .25),2),
+              `Mediana` = round(quantile(imdb, probs = .5),2),
+              `3º Quartil` = round(quantile(imdb, probs = .75),2),
+              `Máximo` = round(max(imdb),2)) %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  latex <- str_c(latex, "| l |\n", sep=" ")
+  for (i in seq(2, col_count))
+  {
+    numCount <- data[i, -c(1)] %>%
+      as.numeric() %>%
+      {floor(log10(.)) + 1} %>%
+      max()
+    latex <- str_c(latex, "\t\t\tS[table-format = ", numCount ,".2]\n", sep="")
+  }
+  
+  
+  latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep="")
+  if (col_count > 2)
+  {
+    for (i in seq(1,col_count))
+    {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+      
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep=" ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep=" ")
+    }
+  }
+  else
+  {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count))
+  {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+  
+  writeLines(latex)
+}
 ### 1) Numero de Lançamentos ----
 # Filtrando o data set
 data_e_lançamentos <- banco %>% select(date_aired,format )
@@ -123,10 +199,10 @@ names(df_imdbeTemp)[names(df_imdbeTemp) == 'season'] <- 'Temporada'
 ggsave(file.path(caminho_andre, "boxplot.pdf"), width = 158, height = 93, units = "mm")
 
 #Dados para medida resumo
-summary(filter(df_imdbeTemp, df_imdbeTemp$Temporada == '1'))
-summary(filter(df_imdbeTemp, df_imdbeTemp$Temporada == '2'))
-summary(filter(df_imdbeTemp, df_imdbeTemp$Temporada == '3'))
-summary(filter(df_imdbeTemp, df_imdbeTemp$Temporada == '4'))
-summary(filter(df_imdbeTemp, df_imdbeTemp$Temporada == 'Especial'))
+df_imdbeTemp %>% 
+  group_by(Temporada) %>%
+  print_quadro_resumo()
+
+
 
 
